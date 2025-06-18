2bf66e05cae96a1f7e47292f2926bab8d96441fc
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-06-17 21:48:15 -0300

    Add files via upload

---
+++ J2025_17_06/DesafioPokemon/Pokemon.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package J2025_17_06.DesafioPokemon;
+
+/**
+ *
+ * @author BRUNOCESARSILVADESOU
+ */
+public class Pokemon {
+    public String nome;
+    public String tipo;
+    public int nivel;
+    
+    public Pokemon(String nome, String tipo, int nivel) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.nivel = nivel;
+    }
+}
