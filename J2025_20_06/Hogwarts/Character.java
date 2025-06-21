309ceb415e79552d255fb6be7920f3c1037582cc
Bruno César <92397493+brunocssouza@users.noreply.github.com>
2025-06-20 21:45:18 -0300

    Add files via upload

---
+++ J2025_20_06/Hogwarts/Character.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package J2025_20_06.Hogwarts;
+
+/**
+ *
+ * @author BRUNOCESARSILVADESOU
+ */
+public class Character {
+
+    
+    public String nome;
+    public String personalidade;
+    /**
+     * @param args the command line arguments
+     */
+    
+    public Character(String nome, String personalidade) {
+        this.nome = nome;
+        this.personalidade = personalidade;
+    }
+    
+}
